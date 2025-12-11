28f1ae104761364b62ae6640e687fecd5bfabf6f
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-12-10 21:45:09 -0300

    Initial monorepo

---
+++ backend/src/main/java/com/trustpay/backend/TrustpayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trustpay.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustpayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustpayBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/trustpay/backend/TrustpayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trustpay.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustpayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
