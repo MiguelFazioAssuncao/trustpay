d6a479dce2daf46d4ea05b65b21f4cd86b948c41
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-12-19 18:21:48 -0300

    feat: created exceptions for transactions

---
+++ backend/src/main/java/com/trustpay/backend/exception/InsufficientBalanceException.java
@@ -0,0 +1,7 @@
+package com.trustpay.backend.exception;
+
+public class InsufficientBalanceException extends RuntimeException {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
---
+++ backend/src/main/java/com/trustpay/backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.trustpay.backend.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
