2ce0e6f0bf31e24c8adc2187bb359f42bd377417
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-12-19 18:09:21 -0300

    feat: added new method for verify if exist role

--- backend/src/main/java/com/trustpay/backend/repository/UserRepository.java
+++ backend/src/main/java/com/trustpay/backend/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.trustpay.backend.repository;
 
 import com.trustpay.backend.entity.User;
+import com.trustpay.backend.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    boolean existsByRole(Role role);
 }
