6c579f22deee4a66a6486b3e50ef0705af89f05c
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-12-19 18:20:50 -0300

    feat: created entity Transaction

---
+++ backend/src/main/java/com/trustpay/backend/entity/Transaction.java
@@ -0,0 +1,48 @@
+package com.trustpay.backend.entity;
+
+import com.trustpay.backend.enums.TransactionStatus;
+import com.trustpay.backend.enums.TransactionType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "transictions")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "from_user_id")
+    private User fromUser;
+
+    @ManyToOne
+    @JoinColumn(name = "to_user_id", nullable = false)
+    private User toUser;
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionStatus status;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
