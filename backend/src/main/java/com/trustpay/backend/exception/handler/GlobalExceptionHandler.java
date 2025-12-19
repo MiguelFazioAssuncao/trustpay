61caf3cd422fa081db84972a546f1fc8f7939857
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-12-19 18:22:16 -0300

    feat: added control of exceptions

--- backend/src/main/java/com/trustpay/backend/exception/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/trustpay/backend/exception/handler/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.trustpay.backend.exception.handler;
 
 import com.trustpay.backend.exception.EmailAlreadyExistsException;
+import com.trustpay.backend.exception.InsufficientBalanceException;
+import com.trustpay.backend.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -41,6 +43,42 @@ public class GlobalExceptionHandler {
                 .body(Map.of(
                         "error", "VALIDATION_ERROR",
                         "fields", fieldErrors
+                ));
+    }
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    public ResponseEntity<Map<String, Object>> handleInsufficientBalance(
+        InsufficientBalanceException ex
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(Map.of(
+                        "error", "INSUFFICIENT_BALANCE",
+                        "message", ex.getMessage()
+                ));
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleResourceNotFound(
+            ResourceNotFoundException ex
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(Map.of(
+                    "error", "NOT_FOUND",
+                    "message", ex.getMessage()
+                ));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, Object>> handleIllegalArgument(
+        IllegalArgumentException ex
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of(
+                        "error", "BAD_REQUEST",
+                        "message", ex.getMessage()
                 ));
     }
 }
