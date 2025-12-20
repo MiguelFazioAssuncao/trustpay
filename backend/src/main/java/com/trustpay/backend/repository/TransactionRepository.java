10ffa732d4034c6f881c833c2b3e896540b3206f
Miguel Fazio de Assunção <faziomiguel0@gmail.com>
2025-12-20 20:50:10 -0300

    feat: added find

--- backend/src/main/java/com/trustpay/backend/repository/TransactionRepository.java
+++ backend/src/main/java/com/trustpay/backend/repository/TransactionRepository.java
@@ -2,11 +2,14 @@ package com.trustpay.backend.repository;
 
 import com.trustpay.backend.entity.Transaction;
 import com.trustpay.backend.entity.User;
+import com.trustpay.backend.enums.TransactionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
     List<Transaction> findByFromUserOrToUser(User from, User to);
+    Optional<Transaction> findTop1ByStatusOrderByCreatedAtAsc(TransactionStatus status);
 }
